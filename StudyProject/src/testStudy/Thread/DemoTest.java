25149aa1ef77b4302295a34c2d72d0d572b5214f
[zhanghao] <[zhang.hao@fujisoft-china.com]>
2019-12-19 13:04:01 +0800

    11

--- StudyProject/src/testStudy/Thread/DemoTest.java
+++ StudyProject/src/testStudy/Thread/DemoTest.java
@@ -1,7 +1,10 @@
 package testStudy.Thread;
 
 public class DemoTest {
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
+        char[] chars = new char[]{'a','b'};
+        byte[] bytes = new byte[]{77,78};
+        String s = new String(bytes);
+        System.out.println(s);
     }
 }
