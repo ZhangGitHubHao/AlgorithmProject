7c96416e30f79f5b63430e1047a9c107ab7ba3c9
[zhanghao] <[zhang.hao@fujisoft-china.com]>
2019-12-25 11:53:39 +0800

    测试一下

--- StudyProject/src/testStudy/CompareObject/DemoTest.java
+++ StudyProject/src/testStudy/CompareObject/DemoTest.java
@@ -3,7 +3,7 @@ package testStudy.CompareObject;
 public class DemoTest {
     public static void main(String[] args) {
         User user1 = new User("张三",12);
-        User user2 = new User("张三",15);
+        User user2 = new User("张三1",15);
         System.out.println(user1.equals(user2));
     }
 }
